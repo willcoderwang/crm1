4c857128a14153ed4ede5c4f7083dfc26bbd78e3
王志坤 <wzk@disroot.org>
2020-02-16 21:54:10 +0800

    1, add Tag model 2, render data to templates

--- accounts/models.py
+++ accounts/models.py
@@ -13,6 +13,13 @@ class Customer(models.Model):
         return self.name
 
 
+class Tag(models.Model):
+    name = models.CharField(max_length=64)
+
+    def __str__(self):
+        return self.name
+
+
 class Product(models.Model):
     CATEGORY = (
         ('Indoor', 'Indoor'),
@@ -22,8 +29,9 @@ class Product(models.Model):
     name = models.CharField(max_length=128)
     price = models.FloatField()
     category = models.CharField(max_length=64, choices=CATEGORY)
-    description = models.CharField(max_length=256)
+    description = models.CharField(max_length=256, blank=True)
     date_created = models.DateTimeField(auto_now_add=True)
+    tags = models.ManyToManyField(Tag)
 
     def __str__(self):
         return self.name
@@ -36,7 +44,7 @@ class Order(models.Model):
         ('Delivered', 'Delivered'),
     )
 
-    # customer =
-    # product =
+    customer = models.ForeignKey(Customer, null=True, on_delete=models.SET_NULL)
+    product = models.ForeignKey(Product, null=True, on_delete=models.SET_NULL)
     date_created = models.DateTimeField(auto_now_add=True)
     status = models.CharField(max_length=64, choices=STATUS)
